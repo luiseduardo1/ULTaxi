2ec2ff0cf55618b765a9be78ff1677fcc60cd975
Luis E. Obando <luiseduardo.obando@gmail.com>
2017-09-21 21:31:43 -0400

    Change project name

--- src/main/java/ca/ulaval/glo4003/ULTaxiMain.java
+++ src/main/java/ca/ulaval/glo4003/ULTaxiMain.java
@@ -27,7 +27,7 @@ import java.util.Set;
  * RESTApi setup without using DI or spring
  */
 @SuppressWarnings("all")
-public class TelephonyWsMain {
+public class ULTaxiMain {
   public static boolean isDev = true; // Would be a JVM argument or in a .property file
 
   public static void main(String[] args)
