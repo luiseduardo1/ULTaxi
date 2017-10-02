00175f8a6265001401a9f85441fa6dd442687fef
Martin L <martin.lessard49@gmail.com>
2017-10-02 17:56:38 -0400

    Add VehicleRepositoryInMemory and VehicleDevDataFactory. Remove InvalidRegistrationNumberException

--- src/main/java/ca/ulaval/glo4003/ws/domain/vehicle/InvalidRegistrationNumberException.java
+++
@@ -1,8 +0,0 @@
-package ca.ulaval.glo4003.ws.domain.vehicle;
-
-public class InvalidRegistrationNumberException extends RuntimeException {
-
-    public InvalidRegistrationNumberException(String message) {
-        super(message);
-    }
-}
