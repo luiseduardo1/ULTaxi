aaa125a013fda302e86ed664bb9b7dee7cfc66c4
Luis E. Obando <luiseduardo.obando@gmail.com>
2017-10-03 03:54:32 -0400

    Add RequestService and tests

--- src/main/java/ca/ulaval/glo4003/ws/domain/request/RequestService.java
+++ src/main/java/ca/ulaval/glo4003/ws/domain/request/RequestService.java
@@ -2,8 +2,22 @@ package ca.ulaval.glo4003.ws.domain.request;
 
 import ca.ulaval.glo4003.ws.api.request.dto.RequestDto;
 
+import java.util.logging.Logger;
+
 public class RequestService {
 
+    private Logger logger = Logger.getLogger(RequestService.class.getName());
+    private RequestRepository requestRepository;
+    private RequestAssembler requestAssembler;
+
+    public RequestService(RequestRepository requestRepository, RequestAssembler requestAssembler) {
+        this.requestRepository = requestRepository;
+        this.requestAssembler = requestAssembler;
+    }
+
     public void sendTransportRequest(RequestDto requestDto) {
+        logger.info("Add new transport request");
+        Request request = requestAssembler.create(requestDto);
+        requestRepository.save(request);
     }
 }
