a7a86641b1ed7ed1137113855a2a05bfaddc9396
Martin L <martin.lessard49@gmail.com>
2017-10-03 14:57:36 -0400

    Add vehicle integration tests + refactor user ITs

--- src/test/java/ca/ulaval/glo4003/ws/UserResourceIT.java
+++ src/test/java/ca/ulaval/glo4003/ws/UserResourceIT.java
@@ -19,30 +19,30 @@ public class UserResourceIT {
 
     private static final int USER_TEST_SERVER_PORT = 8080;
     private static final String A_VALID_NAME = "Ronald";
-    private static final String A_VALID_PASSWORD = "Beaubrun";
     private static final String AN_INVALID_NAME = "ronald.beaubrun@ulaval.ca";
-    private static final String API_USERS = "/api/users";
+    private static final String A_VALID_PASSWORD = "Beaubrun";
+    private static final String USERS_API = "/api/users";
     private static final String URL_BASE = "http://localhost";
 
     @Before
-    public void setUp() throws Exception {
-        Thread t = new Thread(() -> {
+    public void setUp() {
+        Thread thread = new Thread(() -> {
             try {
                 ULTaxiMain.main(new String[] {});
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
-        t.setDaemon(true);
-        t.start();
+        thread.setDaemon(true);
+        thread.start();
     }
 
     @Test
     public void givenUserWithValidName_whenCreateUser_thenUserIsCreated() {
         givenBaseUserServer()
-            .body(givenUser())
+            .body(givenAValidUser())
             .when()
-            .post(API_USERS)
+            .post(USERS_API)
             .then()
             .statusCode(Response.Status.OK.getStatusCode());
     }
@@ -50,14 +50,14 @@ public class UserResourceIT {
     @Test
     public void givenAlreadyExistingUser_whenCreateUser_thenReturnsBadRequest() {
         givenBaseUserServer()
-            .body(givenUser())
+            .body(givenAValidUser())
             .when()
-            .post(API_USERS);
+            .post(USERS_API);
 
         givenBaseUserServer()
-            .body(givenUser())
+            .body(givenAValidUser())
             .when()
-            .post(API_USERS)
+            .post(USERS_API)
             .then()
             .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
     }
@@ -65,9 +65,9 @@ public class UserResourceIT {
     @Test
     public void givenUserWithInvalidName_whenCreateUser_thenReturnsBadRequest() {
         givenBaseUserServer()
-            .body(givenUserWithInvalidName())
+            .body(givenAUserWithInvalidName())
             .when()
-            .post(API_USERS)
+            .post(USERS_API)
             .then()
             .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
     }
@@ -80,18 +80,19 @@ public class UserResourceIT {
             .contentType(ContentType.JSON);
     }
 
-    private String givenUser() {
-        UserDto userDto = new UserDto();
-        userDto.setName(A_VALID_NAME);
-        userDto.setPassword(A_VALID_PASSWORD);
-        Gson gson = new Gson();
-        return gson.toJson(userDto);
+    private String givenAValidUser() {
+        return createUserJSON(A_VALID_NAME, A_VALID_PASSWORD);
     }
 
-    private String givenUserWithInvalidName() {
+    private String givenAUserWithInvalidName() {
+        return createUserJSON(AN_INVALID_NAME, A_VALID_PASSWORD);
+    }
+
+    private String createUserJSON(String name, String password) {
         UserDto userDto = new UserDto();
-        userDto.setName(AN_INVALID_NAME);
-        userDto.setPassword(A_VALID_PASSWORD);
+        userDto.setName(name);
+        userDto.setPassword(password);
+
         Gson gson = new Gson();
         return gson.toJson(userDto);
     }
