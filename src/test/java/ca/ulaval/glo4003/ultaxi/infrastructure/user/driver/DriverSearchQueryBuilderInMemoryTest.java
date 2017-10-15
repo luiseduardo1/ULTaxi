635136dcd278c0d271bbe971f00b91cfc2cf3b6d
Antoine Gagné <gagnantoine@gmail.com>
2017-10-15 13:42:36 -0400

    Replace all beaubrun by macdonald

--- src/test/java/ca/ulaval/glo4003/ultaxi/infrastructure/user/driver/DriverSearchQueryBuilderInMemoryTest.java
+++ src/test/java/ca/ulaval/glo4003/ultaxi/infrastructure/user/driver/DriverSearchQueryBuilderInMemoryTest.java
@@ -46,7 +46,7 @@ public class DriverSearchQueryBuilderInMemoryTest {
         List<Driver> foundDrivers = searchDriver.findAll();
         Driver foundDriver = foundDrivers.get(0);
 
-        Driver expectedDriver = (Driver) createDriver("Ronald", "Beaubrun", "972487086");
+        Driver expectedDriver = (Driver) createDriver("Ronald", "Macdonald", "972487086");
         assertEquals(1, foundDrivers.size());
         assertEquals(expectedDriver.getSin(), foundDriver.getSin());
         assertEquals(expectedDriver.getName(), foundDriver.getName());
@@ -83,7 +83,7 @@ public class DriverSearchQueryBuilderInMemoryTest {
 
     private Map<String, User> givenDrivers() {
         Map<String, User> drivers = new HashMap<>();
-        drivers.put("1", createDriver("Ronald", "Beaubrun", "972487086"));
+        drivers.put("1", createDriver("Ronald", "Macdonald", "972487086"));
         drivers.put("2", createDriver("Marcel", "Lepic", "348624487"));
         drivers.put("3", createDriver("Lord", "Gargamel", "215136193"));
 
