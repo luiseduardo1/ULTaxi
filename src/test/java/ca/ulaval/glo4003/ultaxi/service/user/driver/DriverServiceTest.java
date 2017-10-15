efcb35baff29354c1a4af9255d1f3b1c7504ef7f
Antoine Gagné <gagnantoine@gmail.com>
2017-10-15 13:34:45 -0400

    Change Beaubrun by Macdonald

--- src/test/java/ca/ulaval/glo4003/ultaxi/service/user/driver/DriverServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/ultaxi/service/user/driver/DriverServiceTest.java
@@ -82,7 +82,7 @@ public class DriverServiceTest {
 
     private Map<String, User> givenDrivers() {
         Map<String, User> drivers = new HashMap<>();
-        drivers.put("1", createDriver("Ronald", "Beaubrun", "972487086"));
+        drivers.put("1", createDriver("Ronald", "Macdonald", "972487086"));
         drivers.put("2", createDriver("Marcel", "Lepic", "348624487"));
         drivers.put("3", createDriver("Lord", "Gargamel", "215136193"));
 
