fd3e0ab55f7991048217f32e4d04602627c8013d
Martin L <martin.lessard49@gmail.com>
2017-10-21 18:02:59 -0400

    Add authentication handling to abstract IntegrationTest

--- src/test/java/ca/ulaval/glo4003/ultaxi/integration/IntegrationTest.java
+++ src/test/java/ca/ulaval/glo4003/ultaxi/integration/IntegrationTest.java
@@ -2,6 +2,9 @@ package ca.ulaval.glo4003.ultaxi.integration;
 
 import static io.restassured.RestAssured.given;
 
+import ca.ulaval.glo4003.ultaxi.domain.user.Role;
+import ca.ulaval.glo4003.ultaxi.transfer.user.UserDto;
+import com.google.gson.Gson;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -11,6 +14,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class IntegrationTest {
+
+    private static final String USERS_ROUTE = "/api/users";
+    private static final String USER_AUTHENTICATION_ROUTE = USERS_ROUTE + "/auth";
+    private static final String SIGNIN_ROUTE = USER_AUTHENTICATION_ROUTE + "/signin";
+    private static final String AUTHENTICATION_TOKEN_PREFIX = "Bearer ";
+
+    protected String authenticationToken = getGenericRoleUserAuthenticationToken(Role.Anonymous);
+
+    protected void authenticateAsUser(String username, String password, String email) {
+        authenticationToken = getAuthenticationToken(
+            createUserData(username, password, email)
+        );
+    }
+
+    protected void authenticateAsRole(Role role) {
+        authenticationToken = getGenericRoleUserAuthenticationToken(role);
+    }
 
     protected Response authenticatedPost(String path, String body, String authorizationToken) {
         return executePostRequest(
@@ -54,6 +74,12 @@ public abstract class IntegrationTest {
             .statusCode(status.getStatusCode());
     }
 
+    protected String getGenericRoleUserAuthenticationToken(Role role) {
+        return getAuthenticationToken(
+            createGenericRoleUserData(role)
+        );
+    }
+
     private static final Response executePostRequest(RequestSpecification requestSpecification,
         String body) {
         return requestSpecification
@@ -82,5 +108,31 @@ public abstract class IntegrationTest {
             .contentType(ContentType.JSON)
             .header("Connection", "Close")
             .basePath(path);
+    }
+
+    private static String getAuthenticationToken(String userData) {
+        Response response = executePostRequest(
+            createBasicRequestSpecification(SIGNIN_ROUTE), userData
+        );
+        return AUTHENTICATION_TOKEN_PREFIX + response.getBody().asString();
+    }
+
+    private static final String createGenericRoleUserData(Role role) {
+        String lowercaseRole = role.name().toLowerCase();
+        return createUserData(
+            lowercaseRole + "Username",
+            lowercaseRole + "Password",
+            lowercaseRole + "@ultaxi.ca"
+        );
+    }
+
+    private static final String createUserData(String username, String password, String email) {
+        UserDto userDto = new UserDto();
+        userDto.setUserName(username);
+        userDto.setPassword(password);
+        userDto.setEmail(email);
+
+        Gson gson = new Gson();
+        return gson.toJson(userDto);
     }
 }
