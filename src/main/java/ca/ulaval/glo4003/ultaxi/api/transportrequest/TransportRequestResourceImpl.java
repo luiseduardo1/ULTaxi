fb0311df49330b3ad237927657ff1af06177fa9e
luiseduardo1 <luiseduardo.obando@gmail.com>
2017-10-25 12:37:59 -0400

    Add missing exception to be catched

--- src/main/java/ca/ulaval/glo4003/ultaxi/api/transportrequest/TransportRequestResourceImpl.java
+++ src/main/java/ca/ulaval/glo4003/ultaxi/api/transportrequest/TransportRequestResourceImpl.java
@@ -7,6 +7,7 @@ import ca.ulaval.glo4003.ultaxi.domain.user.driver.Driver;
 import ca.ulaval.glo4003.ultaxi.domain.user.exception.EmptySearchResultsException;
 import ca.ulaval.glo4003.ultaxi.domain.vehicle.exception.InvalidVehicleTypeException;
 import ca.ulaval.glo4003.ultaxi.http.authentication.filtering.Secured;
+import ca.ulaval.glo4003.ultaxi.infrastructure.user.jwt.exception.InvalidTokenException;
 import ca.ulaval.glo4003.ultaxi.service.transportrequest.TransportRequestService;
 import ca.ulaval.glo4003.ultaxi.service.user.UserAuthenticationService;
 import ca.ulaval.glo4003.ultaxi.transfer.transportrequest.TransportRequestDto;
@@ -34,7 +35,7 @@ public class TransportRequestResourceImpl implements TransportRequestResource {
             User user = userAuthenticationService.authenticateFromToken(clientToken);
             String transportRequestId = transportRequestService.sendRequest(transportRequestDto, user.getUsername());
             return Response.status(Response.Status.CREATED).entity(transportRequestId).build();
-        } catch (InvalidVehicleTypeException | InvalidGeolocationException exception) {
+        } catch (InvalidVehicleTypeException | InvalidGeolocationException | InvalidTokenException exception) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
     }
@@ -42,9 +43,8 @@ public class TransportRequestResourceImpl implements TransportRequestResource {
     @Override
     @Secured({Role.DRIVER})
     public Response searchAvailableTransportRequests(String driverToken) {
-        Driver driver = (Driver) userAuthenticationService.authenticateFromToken(driverToken);
-
         try {
+            Driver driver = (Driver) userAuthenticationService.authenticateFromToken(driverToken);
             TransportRequestSearchParameters searchParameters =
                 new TransportRequestSearchParameters(driver.getVehicleType().name());
 
@@ -52,7 +52,7 @@ public class TransportRequestResourceImpl implements TransportRequestResource {
                 new GenericEntity<List<TransportRequestDto>>(transportRequestService.searchBy(searchParameters)) {};
 
             return Response.ok(availableTransportRequests).build();
-        } catch (EmptySearchResultsException exception) {
+        } catch (EmptySearchResultsException | InvalidTokenException exception) {
             return Response.status(Response.Status.NOT_FOUND).entity(exception.getMessage()).build();
         }
     }
