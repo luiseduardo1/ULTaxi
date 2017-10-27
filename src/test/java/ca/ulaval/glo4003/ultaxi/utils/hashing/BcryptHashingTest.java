d25955037a22b6e07d58a45661484490456ffaa4
Martin L <martin.lessard49@gmail.com>
2017-10-27 12:21:45 -0400

    Added tests to BcryptHashingTest for new functionality

--- src/test/java/ca/ulaval/glo4003/ultaxi/utils/hashing/BcryptHashingTest.java
+++ src/test/java/ca/ulaval/glo4003/ultaxi/utils/hashing/BcryptHashingTest.java
@@ -1,5 +1,6 @@
 package ca.ulaval.glo4003.ultaxi.utils.hashing;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
@@ -9,37 +10,67 @@ import org.junit.Test;
 
 public class BcryptHashingTest {
 
-    private static final String A_PASSWORD = "ro|\\|/-\\|8/-\\|8run";
+    private static final String A_VALUE = "ro|\\|/-\\|8/-\\|8run";
     private final HashingStrategy hashingStrategy = new BcryptHashing();
 
     @Test
-    public void givenANullPassword_whenHashing_thenReturnsNull() {
-        String hashedPassword = hashingStrategy.hash(null);
+    public void givenANullValue_whenHashing_thenReturnsNull() {
+        String hashedValue = hashingStrategy.hash(null);
 
-        assertNull(hashedPassword);
+        assertNull(hashedValue);
     }
 
     @Test
-    public void givenAPassword_whenHashing_thenReturnsHashedPassword() {
-        String hashedPassword = hashingStrategy.hash(A_PASSWORD);
+    public void givenAValue_whenHashing_thenReturnsHashedValue() {
+        String hashedValue = hashingStrategy.hash(A_VALUE);
 
-        assertNotEquals(A_PASSWORD, hashedPassword);
+        assertNotEquals(A_VALUE, hashedValue);
     }
 
     @Test
-    public void givenAHashedPassword_whenCheckingIfEqualToTheTheUnhashedPassword_thenReturnsTrue() {
-        String hashedPassword = hashingStrategy.hash(A_PASSWORD);
+    public void givenAValue_whenHashingTwice_thenHashesAreTheSame() {
+        String firstHashedValue = hashingStrategy.hash(A_VALUE);
+        String secondHashedValue = hashingStrategy.hash(A_VALUE);
 
-        boolean areEquals = hashingStrategy.areEquals(A_PASSWORD, hashedPassword);
+        assertEquals(firstHashedValue, secondHashedValue);
+    }
+
+    @Test
+    public void givenANullValue_whenHashingWithRandomSalt_thenReturnsNull() {
+        String hashedValue = hashingStrategy.hashWithRandomSalt(null);
+
+        assertNull(hashedValue);
+    }
+
+    @Test
+    public void givenAValue_whenHashingWithRandomSalt_thenReturnsHashedValue() {
+        String hashedValue = hashingStrategy.hashWithRandomSalt(A_VALUE);
+
+        assertNotEquals(A_VALUE, hashedValue);
+    }
+
+    @Test
+    public void givenAValue_whenHashingWithRandomSaltTwice_thenHashesAreDifferent() {
+        String firstHashedValue = hashingStrategy.hashWithRandomSalt(A_VALUE);
+        String secondHashedValue = hashingStrategy.hashWithRandomSalt(A_VALUE);
+
+        assertNotEquals(firstHashedValue, secondHashedValue);
+    }
+
+    @Test
+    public void givenAHashedValue_whenCheckingIfEqualToTheTheUnhashedValue_thenReturnsTrue() {
+        String hashedValue = hashingStrategy.hash(A_VALUE);
+
+        boolean areEquals = hashingStrategy.areEquals(A_VALUE, hashedValue);
 
         assertTrue(areEquals);
     }
 
     @Test
-    public void givenANullPassword_whenCheckingIfEqualToHashedPassword_thenReturnsFalse() {
-        String hashedPassword = hashingStrategy.hash(A_PASSWORD);
+    public void givenANullValue_whenCheckingIfEqualToHashedValue_thenReturnsFalse() {
+        String hashedValue = hashingStrategy.hash(A_VALUE);
 
-        boolean areEquals = hashingStrategy.areEquals(null, hashedPassword);
+        boolean areEquals = hashingStrategy.areEquals(null, hashedValue);
 
         assertFalse(areEquals);
     }
