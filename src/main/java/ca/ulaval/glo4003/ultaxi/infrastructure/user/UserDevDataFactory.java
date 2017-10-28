a4aaec1e6e8cd6360937fff49e1cc23f6a7118a7
Antoine Gagné <gagnantoine@gmail.com>
2017-10-27 23:16:18 -0400

    Add hashmap of role and types

--- src/main/java/ca/ulaval/glo4003/ultaxi/infrastructure/user/UserDevDataFactory.java
+++ src/main/java/ca/ulaval/glo4003/ultaxi/infrastructure/user/UserDevDataFactory.java
@@ -2,12 +2,21 @@ package ca.ulaval.glo4003.ultaxi.infrastructure.user;
 
 import ca.ulaval.glo4003.ultaxi.domain.user.Role;
 import ca.ulaval.glo4003.ultaxi.domain.user.User;
+import ca.ulaval.glo4003.ultaxi.domain.user.driver.Driver;
 import ca.ulaval.glo4003.ultaxi.utils.hashing.HashingStrategy;
 import jersey.repackaged.com.google.common.collect.Lists;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class UserDevDataFactory {
+public class UserDevDataFactory<T> {
+
+    private final Map<Role, T> userTypesByRoles = new HashMap<>();
+
+    public UserDevDataFactory() {
+        userTypesByRoles.put(Role.DRIVER, Driver.class);
+    }
 
     public List<User> createMockData(HashingStrategy hashingStrategy) {
         List<User> users = Lists.newArrayList();
