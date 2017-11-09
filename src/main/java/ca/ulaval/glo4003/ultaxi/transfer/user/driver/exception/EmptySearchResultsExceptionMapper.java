863215bb6bfcbd819111df4bd3c23bef6c4fffb8
Antoine Gagné <gagnantoine@gmail.com>
2017-11-09 18:44:19 -0500

    Fix merge bug

--- src/main/java/ca/ulaval/glo4003/ultaxi/transfer/user/driver/exception/EmptySearchResultsExceptionMapper.java
+++ src/main/java/ca/ulaval/glo4003/ultaxi/transfer/user/driver/exception/EmptySearchResultsExceptionMapper.java
@@ -1,6 +1,7 @@
 package ca.ulaval.glo4003.ultaxi.transfer.user.driver.exception;
 
-import ca.ulaval.glo4003.ultaxi.domain.user.exception.EmptySearchResultsException;
+
+import ca.ulaval.glo4003.ultaxi.domain.search.exception.EmptySearchResultsException;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
