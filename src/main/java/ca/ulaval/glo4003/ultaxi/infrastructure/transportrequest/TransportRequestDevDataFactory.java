0e2756e0c27aeb2704da65e032d597fa4801fffc
luiseduardo1 <luiseduardo.obando@gmail.com>
2017-11-24 07:22:00 -0500

    Fix transportRequestDevDataFactory

--- src/main/java/ca/ulaval/glo4003/ultaxi/infrastructure/transportrequest/TransportRequestDevDataFactory.java
+++ src/main/java/ca/ulaval/glo4003/ultaxi/infrastructure/transportrequest/TransportRequestDevDataFactory.java
@@ -1,6 +1,8 @@
 package ca.ulaval.glo4003.ultaxi.infrastructure.transportrequest;
 
+import ca.ulaval.glo4003.ultaxi.domain.geolocation.Geolocation;
 import ca.ulaval.glo4003.ultaxi.domain.transportrequest.TransportRequest;
+import ca.ulaval.glo4003.ultaxi.domain.vehicle.VehicleType;
 import jersey.repackaged.com.google.common.collect.Lists;
 
 import java.util.List;
@@ -10,9 +12,9 @@ public class TransportRequestDevDataFactory {
     public List<TransportRequest> createMockData() {
         List<TransportRequest> transportRequests = Lists.newArrayList();
 
-        TransportRequest transportRequest1 = new TransportRequest();
-        transportRequest1.setId("1");
-        transportRequests.add(transportRequest1);
+        Geolocation startingPosition = new Geolocation(38.1223, 43.2332);
+        TransportRequest transportRequest = new TransportRequest("Johny", startingPosition, "A dev transport request", VehicleType.LIMOUSINE);
+        transportRequests.add(transportRequest);
 
         return transportRequests;
     }
