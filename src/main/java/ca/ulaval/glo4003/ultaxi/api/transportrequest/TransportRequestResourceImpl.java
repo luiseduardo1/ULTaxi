0cdd96e8e1c5cd8b1d670bb3ceca06f8e9912000
luiseduardo1 <luiseduardo.obando@gmail.com>
2017-12-15 19:10:32 -0500

    remove blank line

--- src/main/java/ca/ulaval/glo4003/ultaxi/api/transportrequest/TransportRequestResourceImpl.java
+++ src/main/java/ca/ulaval/glo4003/ultaxi/api/transportrequest/TransportRequestResourceImpl.java
@@ -44,7 +44,6 @@ public class TransportRequestResourceImpl implements TransportRequestResource {
                 driverToken)) {
             };
         return Response.ok(availableTransportRequests).build();
-
     }
 
     @Override
